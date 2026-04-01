6cc770dc5cce953ff8b75319c4db01f1b2110771
Shen <254971264+Shenious@users.noreply.github.com>
2026-04-01 09:49:37 -0600

    structured front page

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("cta-button").addEventListener("click", () => {
+    alert("Waitlist coming soon.");
+});
+
+document.getElementById("cta-button-2").addEventListener("click", () => {
+    alert("Waitlist coming soon.");
+});
