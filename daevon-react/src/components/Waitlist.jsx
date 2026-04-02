56c9696fe8f02dac995314824d161f9f4d2ae454
Shen <254971264+Shenious@users.noreply.github.com>
2026-04-02 16:58:04 -0600

    edit to submission spacing

--- daevon-react/src/components/Waitlist.jsx
+++ daevon-react/src/components/Waitlist.jsx
@@ -4,26 +4,41 @@ export default function Waitlist() {
   const [submitted, setSubmitted] = useState(false);
 
   if (submitted) {
-    return <p>Welcome to Daevon. We’ll reach out when we open early access.</p>;
+    return (
+      <div style={{ marginTop: "30px" }}>
+        <p>Welcome to Daevon. We’ll reach out when we open early access.</p>
+      </div>
+    );
   }
 
   return (
-    <form
-      name="waitlist"
-      method="POST"
-      data-netlify="true"
-      onSubmit={() => setSubmitted(true)}
-    >
-      <input type="hidden" name="form-name" value="waitlist" />
+    <div style={{ marginTop: "30px" }}>
+      <form
+        style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center"
+        }}
+        name="waitlist"
+        method="POST"
+        data-netlify="true"
+        onSubmit={() => setSubmitted(true)}
+      >
+        <input type="hidden" name="form-name" value="waitlist" />
 
-      <input
-        type="email"
-        name="email"
-        placeholder="your@email.com"
-        required
-      />
+        <input
+          type="email"
+          name="email"
+          placeholder="your@email.com"
+          required
+          style={{
+            padding: "12px",
+            marginRight: "10px"
+          }}
+        />
 
-      <button type="submit">Join</button>
-    </form>
+        <button type="submit">Join</button>
+      </form>
+    </div>
   );
 }
